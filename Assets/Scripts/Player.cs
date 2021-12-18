e53c302c0629aded3506c39b92b6dbdb0db849b9
jrivera26333 <jrivera26333@gmail.com>
2021-12-17 18:03:28 -0600

    Added Chicken

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Player : MonoBehaviour
 {
-    
+    public delegate void IsDead();
+    public static event IsDead isDeadEvent;
+
+    int health = 5; //Hits
+
+    public void OnDamageTaken(int damage)
+    {
+        health -= damage;
+
+        if (damage <= 0)
+            isDeadEvent?.Invoke();
+    }
 }
--- Assets/Scripts/WorldFlip.cs
+++ Assets/Scripts/WorldFlip.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class WorldFlip : MonoBehaviour
 {
+    [SerializeField] Transform gridTransform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class WorldFlip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+            RotateGrid();
+    }
+
+    public void RotateGrid()
+    {
+        float zGridRotation = gridTransform.localEulerAngles.z;
+        zGridRotation += 180;
+        gridTransform.DORotate(new Vector3(gridTransform.localEulerAngles.x, gridTransform.localEulerAngles.y, zGridRotation), 1.5f).SetEase(Ease.OutSine);
     }
 }
